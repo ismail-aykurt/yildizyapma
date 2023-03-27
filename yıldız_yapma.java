9073cc6119666ea7e73b3dd57740109bb0611f99
ismail <126421146+ismail-aykurt@users.noreply.github.com>
2023-03-27 13:39:17 +0300

    Add files via upload

--- yıldız_yapma.java
+++ yıldız_yapma.java
@@ -7,10 +7,21 @@ public class yıldız_yapma {
         System.out.print("sayı giriniz:");
         number=input.nextInt();
         for(int i=1; i<=number;i++) {
-            for(int k=1; k<=(number-i);k++) {
+            for(int j=1;j<=number-i;j++) {
                 System.out.print(" ");
             }
-            for(int j=1;j<=(2*i)-1;j++) {
+            for(int k=1;k<=(i*2)-1;k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=(number-1);i>=1;i--) {
+            for(int k=1;k<=(number-i);k++) {
+                System.out.print(" ");
+
+
+            }
+            for(int j=(i*2)-1; j>=1;j--){
                 System.out.print("*");
             }
             System.out.println();
